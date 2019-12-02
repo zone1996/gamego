91e94a5a2543ec08b2086b65ca58b0a7ccbc9599
mrmrPaul <2497074816@qq.com>
2019-12-02 18:13:18 +0800

    Now still can run.Just not correctly.

--- DefaultHandler.go
+++ DefaultHandler.go
@@ -1,13 +1,15 @@
 package main
 
 import (
-	cs "gamego/cmd"
+	cmds "gamego/cmd"
 	"gamego/netya"
 
 	log "github.com/zone1996/logo"
 )
 
-type DefaultHandler struct{}
+type DefaultHandler struct {
+	acceptor *netya.Acceptor
+}
 
 func (h *DefaultHandler) OnConnected(session *netya.IoSession) {
 	log.Info("Session ? Connected.", session.Id)
@@ -16,8 +18,8 @@ func (h *DefaultHandler) OnConnected(session *netya.IoSession) {
 func (h *DefaultHandler) OnMessage(session *netya.IoSession, msg *netya.PbMsg) {
 	code := msg.Code
 
-	if c, ok := cs.GetCmd(code); ok {
-		c.Exec(session, msg)
+	if c, ok := cmds.GetCmd(code); ok {
+		session.AddTask(wrapTask(c, session, msg))
 	} else {
 		log.Info("Cmd not found, code:?", code)
 	}
@@ -26,4 +28,11 @@ func (h *DefaultHandler) OnMessage(session *netya.IoSession, msg *netya.PbMsg) {
 func (h *DefaultHandler) OnDisconnected(session *netya.IoSession) {
 	session.Close()
 	log.Info("Session ? DisConnected.", session.Id)
+}
+
+func wrapTask(cmd cmds.Cmd, session *netya.IoSession, msg *netya.PbMsg) func() {
+	f := func() {
+		cmd.Exec(session, msg)
+	}
+	return f
 }
